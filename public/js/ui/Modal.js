b0d2ce34376339542914a9375ff0aeea575fa40f
yulia elkina <todg@mail.ru>
2021-03-31 10:27:00 +0300

    sidebar, modals

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,47 +1,35 @@
-/**
- * Класс Modal отвечает за
- * управление всплывающими окнами.
- * В первую очередь это открытие или
- * закрытие имеющихся окон
- * */
+
 class Modal {
-  /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью Modal.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
-   * */
+
   constructor(element){
+    if (element) {
+      this.element = element;
+      this.registerEvents();
+    }
+    else {
+      console.log("Элемент не существует");
+    };
+  };
 
-  }
 
-  /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
-   * */
   registerEvents() {
+    this.element.querySelectorAll('[data-dismiss="modal"]').forEach(element => { 
+      element.addEventListener("click", (event) => {this.onClose(event);});
+    });
+  };
 
-  }
 
-  /**
-   * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
-   * */
   onClose(e) {
+    e.preventDefault();
+    this.close();
 
-  }
-  /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
-   * */
+  };
+
   open() {
+    this.element.style.display = "block";
+  };
 
-  }
-  /**
-   * Закрывает окно: удаляет CSS-свойство display
-   * */
-  close(){
-
+  close() {
+    this.element.style.display = "";
   }
 }
--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -1,26 +1,16 @@
-/**
- * Класс Sidebar отвечает за работу боковой колонки:
- * кнопки скрытия/показа колонки в мобильной версии сайта
- * и за кнопки меню
- * */
 class Sidebar {
-  /**
-   * Запускает initAuthLinks и initToggleButton
-   * */
+
   static init() {
     this.initAuthLinks();
     this.initToggleButton();
-  }
+  };
 
-  /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
-   * */
   static initToggleButton() {
-
-  }
-
+    document.querySelector(".sidebar-toggle").addEventListener("click", ()=> {
+      document.querySelector("body").classList.toggle("sidebar-open");
+      document.querySelector("body").classList.toggle("sidebar-collapse");
+  });
+  };
   /**
    * При нажатии на кнопку входа, показывает окно входа
    * (через найденное в App.getModal)
@@ -29,6 +19,19 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
-
+    document.querySelector(".menu-item_login").addEventListener("click", () => {
+      const logIn = new Modal(document.getElementById("modal-login"));
+      logIn.open();
+    });
+    document.querySelector(".menu-item_register").addEventListener("click", () => {
+      const registrationModal = new Modal(document.getElementById("modal-register"));
+      registrationModal.open();
+    });
+    document.querySelector(".menu-item_logout").addEventListener("click", () => {
+      User.logout();
+      App.setState("init");
+    }); 
   }
-}
+};
+
+Sidebar.init();
